84265a9833a4853bd8a5d7a08d916e52df63c4e7
czl <605165993@qq.com>
2016-04-11 13:50:13 +0800

    add x86 version

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
-# idaemu
-idaemu is an IDA Pro Plugin - use for emulating code in IDA Pro. it is base on unicorn.
+idaemu
+==============
 
+idaemu is an IDA Pro Plugin - use for emulating code in IDA Pro. it is base on [unicorn-engine](http://www.unicorn-engine.org).  
+
+Support architecture:
+- X86 (16, 32, 64-bit) 
+- ARM (developing)
+
+Now it is not support call the library functions.
+
+Install
+-------
+
+If you want to use idaemu, you have to install [unicorn-engine](http://www.unicorn-engine.org) and Python binding first. Then use the `idaemu.py` as the idapython script.  
+
+
+License
+-------
+
+This project is released under the [GPL license](COPYING).
+
+
+Example
+-------
+
+This is easy function for add. 
+```
+.text:000000000040052D                 public myadd
+.text:000000000040052D myadd           proc near               ; CODE XREF: main+1Bp
+.text:000000000040052D
+.text:000000000040052D var_4           = dword ptr -4
+.text:000000000040052D
+.text:000000000040052D                 push    rbp
+.text:000000000040052E                 mov     rbp, rsp
+.text:0000000000400531                 mov     [rbp+var_4], edi
+.text:0000000000400534                 mov     edx, cs:magic	; magic dd 64h 
+.text:000000000040053A                 mov     eax, [rbp+var_4]
+.text:000000000040053D                 add     eax, edx
+.text:000000000040053F                 pop     rbp
+.text:0000000000400540                 retn
+.text:0000000000400540 myadd           endp
+```
+
+Running the idapython scritp:
+``` python
+from idaemu import *
+a = Emu(UC_ARCH_X86, UC_MODE_64)
+a.eFunc(0x040052D, 7)
+```
+
+Get the function result:
+```
+Euclation done. Below is the Result:
+>>> function result = 107
+```
---
+++ idaemu.py
@@ -0,0 +1,101 @@
+from __future__ import print_function
+from unicorn import *
+from unicorn.x86_const import *
+from struct import pack
+from idaapi import get_func
+from idc import Qword
+
+
+PAGE_ALIGN = 1024 * 1024 # 1m
+
+COMPILE_GCC = 1
+COMPILE_MSVC = 2
+
+class Emu(object):    
+    def __init__(self, arch, mode, compiler=COMPILE_GCC, stack=0xf000000, ssize=3):
+        assert(arch in [UC_ARCH_X86])
+        self.arch = arch
+        self.mode = mode
+        self.compiler = compiler
+        self.stack = self._alignAddr(stack)
+        self.ssize = ssize
+    
+    # callback for tracing invalid memory access (READ or WRITE)
+    def _hook_mem_invalid(self, uc, access, address, size, value, user_data):
+        addr = self._alignAddr(address)
+        uc.mem_map(addr, PAGE_ALIGN)
+        data = self._getOriginData(addr, PAGE_ALIGN)
+        uc.mem_write(addr, data)
+        return True
+
+    def _alignAddr(self, addr):
+        return addr // PAGE_ALIGN * PAGE_ALIGN
+
+    def _getOriginData(self, address, size):
+        res = [pack("<Q", Qword(address + offset)) for offset in xrange(0, size+8, 8)]
+        res = "".join(res)
+        return res[:size]
+
+    def _initStackAndArgs(self, uc, RA, *args):
+        uc.mem_map(self.stack, (self.ssize+1) * PAGE_ALIGN)
+        sp = self.stack + self.ssize * PAGE_ALIGN
+        regs = []
+        if self.mode == UC_MODE_16:
+            step = 2
+            uc.reg_write(UC_X86_REG_SP, sp)
+            uc.mem_write(sp, pack('<H', RA))
+            self.RES_REG = UC_X86_REG_AX
+        elif self.mode == UC_MODE_32:
+            step = 4
+            uc.reg_write(UC_X86_REG_ESP, sp)
+            uc.mem_write(sp, pack('<I', RA))
+            self.RES_REG = UC_X86_REG_EAX
+        elif self.mode == UC_MODE_64:
+            step = 8
+            uc.reg_write(UC_X86_REG_RSP, sp)
+            uc.mem_write(sp, pack('<Q', RA))
+            self.RES_REG = UC_X86_REG_RAX
+            if self.compiler == COMPILE_GCC:
+                regs = [UC_X86_REG_RDI, UC_X86_REG_RSI, UC_X86_REG_RDX, UC_X86_REG_RCX, 
+                        UC_X86_REG_R8, UC_X86_REG_R9]
+            elif self.compiler == COMPILE_MSVC:
+                regs = [UC_X86_REG_RCX, UC_X86_REG_RDX, UC_X86_REG_R8, UC_X86_REG_R9]
+        
+        ## init the arguments
+        i = 0
+        while i < len(regs) and i < len(args):
+            uc.reg_write(regs[i], args[i])
+            i += 1
+        while i < len(args):
+            sp += step
+            uc.mem_write(sp, args[i])
+            i += 1
+
+    def eFunc(self, address, *args):
+        func = get_func(address)
+        funcSize = func.endEA - func.startEA
+        try:
+            uc = Uc(self.arch, self.mode)
+            # init code
+            addr = self._alignAddr(func.startEA)
+            size = PAGE_ALIGN
+            while addr + size <= func.endEA: size += PAGE_ALIGN
+            uc.mem_map(addr, size)
+            code = self._getOriginData(func.startEA, funcSize)
+            uc.mem_write(func.startEA, code)
+
+            RA = addr + size - 1 # return addr, for stop emulate
+            self._initStackAndArgs(uc, RA, *args)
+
+            # add the invalid memory access hook
+            uc.hook_add(UC_HOOK_MEM_READ_UNMAPPED | UC_HOOK_MEM_WRITE_UNMAPPED, \
+                        self._hook_mem_invalid)
+            # start emulate
+            uc.emu_start(func.startEA, RA)
+            
+            print("Euclation done. Below is the Result:")
+            res = uc.reg_read(self.RES_REG)
+            print(">>> function result = %d" % res)
+        except UcError as e:
+            print("#ERROR: %s" % e)
+        
